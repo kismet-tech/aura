c2f5bb325c5696afd694bb10644f6869c1416eff
Julian Theoderik Trajanson <julian.trajanson@gmail.com>
2024-12-18 16:20:49 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./node_modules/@radix-ui/aura/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-day-picker/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       borderRadius: {
